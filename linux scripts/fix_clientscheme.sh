cf2459ee8e71ee736f5317ec9e680a2186afb85f
Jack Kennedy <quickkennedy@gmail.com>
2025-05-03 18:34:51 -0400

    fix default fonts
    this MIGHT fix the game crashing? idk.

--- linux scripts/fix_clientscheme.sh
+++ linux scripts/fix_clientscheme.sh
@@ -19,4 +19,16 @@ sed -i 's/\.ttf"/\.otf"/g' "$file"
 # Replace .otf" with -fixed.otf"
 sed -i 's/\.otf"/-fixed.otf"/g' "$file"
 
+# HACK --- FIX DEFAULT FONTS
+sed -i \
+    -e 's/ocra-fixed\.otf/ocra.ttf/Ig' \
+    -e 's/tf2build-fixed\.otf/tf2build.ttf/Ig' \
+    -e 's/tf2professor-fixed\.otf/tf2professor.ttf/Ig' \
+    -e 's/tf2secondary-fixed\.otf/tf2secondary.ttf/Ig' \
+    -e 's/tf2-fixed\.otf/tf2.ttf/Ig' \
+    -e 's/tfd-fixed\.otf/tfd.ttf/Ig' \
+    -e 's/tflogo-fixed\.otf/tflogo.ttf/Ig' \
+    -e 's/tf-fixed\.otf/tf.ttf/Ig' \
+    "$file"
+
 echo "Replacements completed in $file"
